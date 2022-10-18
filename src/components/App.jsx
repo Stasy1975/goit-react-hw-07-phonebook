8f945fea1ef22a33bc97cfe92409b1e67ffd7a4a
Anastasiia Nedbai <nedbayfomina@gmail.com>
2022-10-18 23:59:58 +0300

    phone3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,10 +9,10 @@ import Filter from './Filter/Filter'
 class App extends Component {
 state = {
   contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+    // {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    // {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    // {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    // {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
   ],
   filter: '',
 }
@@ -42,6 +42,19 @@ state = {
   findContact = searchName => {
     this.setState({ filter: searchName });
   };
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'))
+    if (contacts){
+      this.setState({contacts: contacts})
+    }
+    
+}
+
+  componentDidUpdate(prevProps, prevState) {
+      if (this.state.contacts !== prevState.contacts)
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+
 
   render() {
     const normalizedFilter = this.state.filter.toLowerCase();
